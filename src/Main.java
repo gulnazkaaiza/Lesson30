a99aec6ef8622da0d4bf894aea41fc8296e6d90e
Gulnaz Aizatullina <gulnazka.aiza@gmail.com>
2020-09-14 16:36:05 +0200

    Задача выполнена

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Main task = new Main();
+        task.run();
+    }
+
+    public void run () {
+        Methods.analyzeWord(Methods.takeWord());
+    }
+}
